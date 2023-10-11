5efa00e53100758cb2e0c1a904b48e195284586b
Roger <rogersalas2000@gmail.com>
2023-10-11 10:22:01 +0200

    conexio amb 2 escenes diferents

--- Assets/PRACTIQUES/fool.cs
+++ Assets/PRACTIQUES/fool.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class fool : MonoBehaviour
 {
+    [SerializeField] string SceneName = "New Scene";
+
     void Update()
     {
-        transform.Rotate(Vector3.forward * 10.0f);
+        //transform.Rotate(Vector3.forward * 10.0f);
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneName);
     }
 }
