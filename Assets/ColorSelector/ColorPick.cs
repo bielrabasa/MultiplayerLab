1e79482b1e84537c8e6151fe224ab647d1232911
Roger Salas <rogersalas2000@gmail.com>
2023-12-20 18:11:50 +0100

    starting color pick tank

---
+++ Assets/ColorSelector/ColorPick.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class ColorPickButton : MonoBehaviour
+{
+    //https://www.youtube.com/watch?v=M6kEW6feh7g
+    public UnityEvent<Color> ColorPickerEvent;
+    
+    [SerializeField] Texture2D colorChart;
+    [SerializeField] GameObject chart;
+    [SerializeField] RectTransform cursor;
+    [SerializeField] Image button;
+    [SerializeField] Image cursorColor;
+    public void PickColor(BaseEventData data)
+    {
+        PointerEventData pointer = data as PointerEventData;
+
+        cursor.position = pointer.position;
+
+        Color pickedColor = colorChart.GetPixel((int)(cursor.localPosition.x * (colorChart.width / transform.GetChild(0).GetComponent<RectTransform>().rect.width)), (int)(cursor.localPosition.y * (colorChart.height / transform.GetChild(0).GetComponent<RectTransform>().rect.height)));
+        
+        button.color = pickedColor;
+        cursorColor.color = pickedColor;
+        ColorPickerEvent.Invoke(pickedColor);
+    }
+}
