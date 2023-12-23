25355bba83850c989f4d5d8573a8aa9c105ff972
Roger Salas <rogersalas2000@gmail.com>
2023-12-23 19:49:41 +0100

    color selector (not select well)

---
+++ Assets/ColorSelector/ColorTank.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorTank : MonoBehaviour
+{
+    public void SetColor(Color newColor)
+    {
+        GetComponent<Image>().color = newColor;
+    }
+}
